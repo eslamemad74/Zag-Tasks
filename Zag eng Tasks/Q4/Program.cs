628c2de9fc32674a4a413f7cbc3d52a50a1aa8d3
eslamemad74 <eslamemad2026@gmail.com>
2026-02-09 23:24:14 +0200

    Add files via upload

---
+++ Zag eng Tasks/Q4/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Q4
+{
+
+    class User
+    {
+        public string Name;
+    }
+
+    struct UserSnapshot
+    {
+        public string Name;
+    }
+
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User u1 = new User();
+            u1.Name = "Eslam";
+
+            UserSnapshot s1 = new UserSnapshot();
+            s1.Name = "Eslam";
+
+            
+            Modify(u1, s1);
+
+            Console.WriteLine("After call:");
+            Console.WriteLine("User Name: " + u1.Name);
+            Console.WriteLine("Snapshot Name: " + s1.Name);
+
+            Console.WriteLine();
+
+            
+            ModifyByRef(ref u1, ref s1);
+
+            Console.WriteLine("After ref call:");
+            Console.WriteLine("User Name: " + u1.Name);
+            Console.WriteLine("Snapshot Name: " + s1.Name);
+        }
+
+        static void Modify(User user, UserSnapshot snapshot)
+        {
+            user.Name = "Ahmed";
+            snapshot.Name = "Ahmed";
+        }
+
+        static void ModifyByRef(ref User user, ref UserSnapshot snapshot)
+        {
+            user.Name = "sama";
+            snapshot.Name = "sama";
+        }
+    }
+}
---
+++ Zag eng Tasks/Q5/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Q5
+{
+    class InsufficientBalanceException : Exception
+    {
+        public InsufficientBalanceException(string message) : base(message)
+        {
+
+        }
+    }
+    class PaymentTimeoutException : Exception
+    {
+       public PaymentTimeoutException(string message) : base(message)
+       {
+
+       }
+    }
+
+     internal class Program
+     {
+        static void Main(string[] args)
+        {
+           try
+           {
+                    
+              MakePayment(100, 150);
+           }
+           catch (InsufficientBalanceException ex)
+           {
+              Console.WriteLine("Balance Error: " + ex.Message);
+           }
+           catch (PaymentTimeoutException ex)
+           {
+              Console.WriteLine("Timeout Error: " + ex.Message);
+           }
+           catch (Exception ex)
+           {
+              Console.WriteLine("General Error: " + ex.Message);
+           }
+           finally
+           {
+              Console.WriteLine("Payment process finished");
+           }
+        }
+
+         static void MakePayment(int balance, int amount)
+         {
+            if (balance < amount)
+            {
+              throw new InsufficientBalanceException("Not enough balance");
+            }
+
+            bool timeout = true;
+
+            if (timeout)
+            {
+                throw new PaymentTimeoutException("Payment took too long");
+            }
+
+            Console.WriteLine("Payment successful");
+         }
+     }
+}
+  
